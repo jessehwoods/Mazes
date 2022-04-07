ac27d7a8d8a4b52f1869b5928af0b1e022f30255
Jesse Woods <jessehwoods@gmail.com>
2022-04-07 15:06:09 -0400

    Started maze interface class.

---
+++ src/maze/twoDeeOrthoMaze.java
@@ -0,0 +1,16 @@
+package maze;
+
+/**
+ * Interface defining the behavior of a maze that can be accessed as a group of
+ * nodes arranged on a 2d plane, with cartesian coordinates (row, column), only 
+ * orthogonal (up, down, left, right) connections, and all paths can be assumed 
+ * to be two-way.
+ * 
+ * @author Jesse Woods
+ *
+ */
+public interface twoDeeOrthoMaze {
+
+	
+	
+}
