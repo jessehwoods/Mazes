1f2c792478479f996e1102ba52081e0d3d415f77
Jesse Woods <jessehwoods@gmail.com>
2022-04-14 15:33:13 -0400

    Started test case for writer object.

---
+++ test/io/twoDeeOrthoMazeWriterTest.java
@@ -0,0 +1,14 @@
+package io;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class twoDeeOrthoMazeWriterTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
